fe7537deb941a7839481d22483b02e4e0a3310ba
Hamza Amar <rhamzax@gmail.com>
2022-07-20 16:20:27 -0600

    Create an operate function to determine the sum

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
+
+function operate(operator, num1, num2){
+    let sum = 0;
+    if (operator === "add"){
+        sum = add(num1, num2);
+    }
+    else if (operator === "subtract"){
+        sum = subtract(num1, num2);
+    }
+    else if (operator === "multiply"){
+        sum = multiply(num1, num2);
+    }
+    else if (operator === "divide"){
+        sum = divide(num1, num2);
+    }
+}
